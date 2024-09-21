a1f035fdb78da54a302fca34d3336f53f32b01db
Jacques Mayol <jacques@ocean.sea>
2024-09-21 16:34:53 +0200

    Format async tcp echo server

--- main.cpp
+++ main.cpp
@@ -8,110 +8,105 @@
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 //
 
+#include <boost/asio/ts/buffer.hpp>
+#include <boost/asio/ts/internet.hpp>
 #include <cstdlib>
 #include <iostream>
 #include <memory>
 #include <utility>
-#include <boost/asio/ts/buffer.hpp>
-#include <boost/asio/ts/internet.hpp>
 
 using boost::asio::ip::tcp;
 
-class session
-  : public std::enable_shared_from_this<session>
+class session : public std::enable_shared_from_this<session>
 {
 public:
-  session(tcp::socket socket)
-    : socket_(std::move(socket))
-  {
-  }
+    session(tcp::socket socket)
+        : socket_(std::move(socket))
+    {
+    }
 
-  void start()
-  {
-    do_read();
-  }
+    void start() { do_read(); }
 
 private:
-  void do_read()
-  {
-    auto self(shared_from_this());
-    socket_.async_read_some(boost::asio::buffer(data_, max_length),
-        [this, self](boost::system::error_code ec, std::size_t length)
-        {
-          if (!ec)
-          {
-            do_write(length);
-          }
-        });
-  }
+    void do_read()
+    {
+        auto self(shared_from_this());
+        socket_.async_read_some(
+            boost::asio::buffer(data_, max_length), [this, self](boost::system::error_code ec, std::size_t length) {
+                if (!ec)
+                {
+                    do_write(length);
+                }
+            });
+    }
 
-  void do_write(std::size_t length)
-  {
-    auto self(shared_from_this());
-    boost::asio::async_write(socket_, boost::asio::buffer(data_, length),
-        [this, self](boost::system::error_code ec, std::size_t /*length*/)
-        {
-          if (!ec)
-          {
-            do_read();
-          }
-        });
-  }
+    void do_write(std::size_t length)
+    {
+        auto self(shared_from_this());
+        boost::asio::async_write(socket_, boost::asio::buffer(data_, length),
+            [this, self](boost::system::error_code ec, std::size_t /*length*/) {
+                if (!ec)
+                {
+                    do_read();
+                }
+            });
+    }
 
-  tcp::socket socket_;
-  enum { max_length = 1024 };
-  char data_[max_length];
+    tcp::socket socket_;
+    enum
+    {
+        max_length = 1024
+    };
+    char data_[max_length];
 };
 
 class server
 {
 public:
-  server(boost::asio::io_context& io_context, short port)
-    : acceptor_(io_context, tcp::endpoint(tcp::v4(), port)),
-      socket_(io_context)
-  {
-    do_accept();
-  }
-
-private:
-  void do_accept()
-  {
-    acceptor_.async_accept(socket_,
-        [this](boost::system::error_code ec)
-        {
-          if (!ec)
-          {
-            std::make_shared<session>(std::move(socket_))->start();
-          }
-
-          do_accept();
-        });
-  }
-
-  tcp::acceptor acceptor_;
-  tcp::socket socket_;
-};
-
-int main(int argc, char* argv[])
-{
-  try
-  {
-    if (argc != 2)
+    server(boost::asio::io_context & io_context, short port)
+        : acceptor_(io_context, tcp::endpoint(tcp::v4(), port))
+        , socket_(io_context)
     {
-      std::cerr << "Usage: async_tcp_echo_server <port>\n";
-      return 1;
+        do_accept();
     }
 
-    boost::asio::io_context io_context;
+private:
+    void do_accept()
+    {
+        acceptor_.async_accept(socket_, [this](boost::system::error_code ec) {
+            if (!ec)
+            {
+                std::make_shared<session>(std::move(socket_))->start();
+            }
 
-    server s(io_context, std::atoi(argv[1]));
+            do_accept();
+        });
+    }
 
-    io_context.run();
-  }
-  catch (std::exception& e)
-  {
-    std::cerr << "Exception: " << e.what() << "\n";
-  }
+    tcp::acceptor acceptor_;
+    tcp::socket socket_;
+};
 
-  return 0;
+int main(int argc, char * argv[])
+{
+    try
+    {
+        if (argc != 2)
+        {
+            std::cerr << "Usage: async_tcp_echo_server <port>\n";
+            return 1;
+        }
+
+        boost::asio::io_context io_context;
+
+        server s(io_context, std::atoi(argv[1]));
+
+        io_context.run();
+    }
+    catch (std::exception & e)
+    {
+        std::cerr << "Exception: " << e.what() << "\n";
+    }
+
+    return 0;
 }
